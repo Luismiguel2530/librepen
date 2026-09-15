2a3eeed713708afff35105bcaee29f0d9589c6d9
Luis <luiswayne2530@gmail.com>
2026-09-15 00:08:26 -0400

    feat: add Emmet support for HTML and CSS

--- src/components/editors/CodeEditor.jsx
+++ src/components/editors/CodeEditor.jsx
@@ -1,6 +1,22 @@
-import Editor from "@monaco-editor/react";
+import { useEffect, useRef } from "react";
+import Editor, { useMonaco } from "@monaco-editor/react";
+import { emmetCSS, emmetHTML } from "emmet-monaco-es";
 
 function CodeEditor({ language, value, onChange }) {
+  const monaco = useMonaco();
+  const emmetInitialized = useRef(false);
+
+  useEffect(() => {
+    if (!monaco || emmetInitialized.current) {
+      return;
+    }
+
+    emmetHTML(monaco, ["html"]);
+    emmetCSS(monaco, ["css"]);
+
+    emmetInitialized.current = true;
+  }, [monaco]);
+
   return (
     <Editor
       height="100%"
@@ -12,10 +28,21 @@ function CodeEditor({ language, value, onChange }) {
         minimap: {
           enabled: false,
         },
+
         fontSize: 14,
         automaticLayout: true,
         scrollBeyondLastLine: false,
         wordWrap: "on",
+
+        quickSuggestions: {
+          other: true,
+          comments: false,
+          strings: true,
+        },
+
+        suggestOnTriggerCharacters: true,
+        tabCompletion: "on",
+        wordBasedSuggestions: "currentDocument",
       }}
     />
   );
